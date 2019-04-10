c0f7c343a9d44b3969ed1140aa0445f1672fe459
ryanboris <ryan.boris@gmail.com>
2019-04-09 21:27:00 -0400

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { render, cleanup } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+afterEach(cleanup)
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  render(<App />)
+})
