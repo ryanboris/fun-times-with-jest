68a4d923a3221c44c1bcccedd6ba9e706cef5c20
ryanboris <ryan.boris@gmail.com>
2019-04-09 22:15:21 -0400

    initial commit on branch react-testing

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+const App = () => (
+  <>
+    <h1>Sanity!</h1>
+  </>
+)
+
 ReactDOM.render(<App />, document.getElementById('root'))
